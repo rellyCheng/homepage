153b26e6d54e424c66c2803d8f58822dffd3766c
Relly Cheng <rellycheng@RellydeMacBook-Pro.local>
2019-10-23 17:09:35 +0800

    first commit

---
+++ build/precache-manifest.9c247396e1743a0b2a7a38292ac35598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53c3d868f73b9a53fd723707632074d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8563312ef9b3fe256568",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bb0a6e7b939a5ea1d58e",
+    "url": "/static/js/2.fc14e267.chunk.js"
+  },
+  {
+    "revision": "8563312ef9b3fe256568",
+    "url": "/static/js/main.cb6ac304.chunk.js"
+  },
+  {
+    "revision": "c767e08bc74adc2b6de1",
+    "url": "/static/js/runtime-main.4d4056f6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
